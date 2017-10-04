596293939401a0c9edbbce9e53a2918c90737e34
somak <somak.das2@cognizant.com>
2017-10-04 15:21:53 +0000

    Advanced dynamic form flow

---
+++ AdvancedFormflow/RestaurantDemo/Model/MenuDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantDemo.Model
+{
+    public class MenuDB
+    {
+        public static List<MenuOption> GetAllMenuOptions()
+        {
+            return new List<MenuOption>() {
+                new MenuOption(){ItemName = "CrispyChicken" }, 
+                new MenuOption(){ ItemName = "ChickenWings" },
+                new MenuOption(){ ItemName = "ChickenDrumStick" },
+                new MenuOption(){ ItemName = "ChickenPopcorn" },
+            };
+        }
+    }
+}
---
+++ AdvancedFormflow/RestaurantDemo/Model/OrderDetails.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Bot.Builder.FormFlow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow.Advanced;
+using System.Threading.Tasks;
+
+namespace RestaurantDemo.Model
+{
+    [Serializable]
+    public class OrderDetails
+    {
+        public List<MenuOption> MenuItems { get; set; }
+
+        public DeliveryOptions DeliveryMode { get; set; }
+
+        public string UserName { get; set; }
+        public string Phone { get; set; }
+        public string address { get; set; }
+
+
+        public static IForm<OrderDetails> BuildForm()
+        {
+
+            var menuItems = MenuDB.GetAllMenuOptions();
+
+            var builder = new FormBuilder<OrderDetails>();
+
+            builder
+            .Message("Welcome to demo Restaurant bot!")
+            .Field(new FieldReflector<OrderDetails>(nameof(MenuItems))
+            .SetType(null)
+            .SetDefine((state, field) =>
+            {
+                foreach (var item in menuItems)
+                {
+                    field
+                    .AddDescription(item, item.ItemName)
+                    .AddTerms(item, item.ItemName);
+                }
+
+                return Task.FromResult(true);
+            }))
+            .AddRemainingFields()
+            .OnCompletion(async (context, order) =>
+            {
+                await context.PostAsync("Thanks for your order!");
+            });
+
+            return builder.Build();
+        }
+
+    }
+    // This starts at 1 because 0 is the "no value" value
+    public enum DeliveryOptions
+    {
+        TakeAway = 1,
+        Delivery
+    }
+
+    [Serializable]
+    public class MenuOption
+    {
+        public string ItemName { get; set; }
+        public string Description { get; set; }
+        public string ItemImage { get; set; }
+    }
+
+
+}
